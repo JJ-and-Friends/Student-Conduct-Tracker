d2e6211245c8f56585a6854762cdca280248535d
DominiqueDavid <109325294+DominiqueDavid@users.noreply.github.com>
2023-11-29 22:54:51 +0000

    Updated karma views

--- App/views/karma.py
+++ App/views/karma.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from App.database import db
+from App.models import Karma, Student, UpdateRankStrategy
 from App.controllers import Student
 
 from App.controllers.karma import (
@@ -12,5 +13,13 @@ karma_views = Blueprint("karma_views", __name__, template_folder='../templates')
 # Route to update Karma rankings for all students
 @karma_views.route("/karma/update_rankings", methods=["POST"])
 def update_karma_rankings_route():
-    update_student_karma_rankings()
+    update_rank_strategy = UpdateRankStrategy()
+    update_rank_strategy.execute()
     return "Karma rankings updated", 200
+
+"""
+# Route to update Karma rankings for all students
+@karma_views.route("/karma/update_rankings", methods=["POST"])
+def update_karma_rankings_route():
+    update_student_karma_rankings()
+    return "Karma rankings updated", 200"""
